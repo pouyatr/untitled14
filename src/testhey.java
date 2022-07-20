c0d87d14acc17efc7743995924a126530b1db593
pouyatr <pouya.tr01@gmail.com>
2022-07-20 15:01:43 +0430

    Initial commit

---
+++ src/testhey.java
@@ -0,0 +1,7 @@
+public class testhey {
+    public static void main(String[] args){
+        System.out.println("hey");
+        System.out.println("you");
+        System.out.println("pink");
+        System.out.println("floyd");}
+}
